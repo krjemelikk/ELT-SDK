e456f0fda3228d4bfc0f57198171317a2c100d9e
Mikhail Eltsov <michael.eltsov@gmail.com>
2025-04-29 20:32:40 +0300

    replace products data from json to csv

--- Assets/ELTSDK/Source/Services/EditorServices/EditorIAPService.cs
+++ Assets/ELTSDK/Source/Services/EditorServices/EditorIAPService.cs
@@ -12,16 +12,18 @@ namespace ELTSDK.Source.Services.EditorServices
 {
    internal class EditorIAPService : IIAPService
    {
-      private const string EditorProductPath = "products";
+      private const string ProductsCsvPath = "products";
+      private const string CurrencyURL =
+         "//yastatic.net/s3/games-static/static-data/images/payments/sdk/currency-icon-s@2x.png";
+
       public Dictionary<string, Product> Products { get; private set; } = new();
       public Sprite CurrencySprite { get; private set; }
       public event Action<string> PurchaseComplete;
 
       public async UniTask LoadAllProductData()
       {
-         var json = Resources.Load<TextAsset>(EditorProductPath).text;
-         var products = JsonConvert.DeserializeObject<List<Product>>(json);
-         Products = products.ToDictionary(x => x.Id, x => x);
+         var csv = Resources.Load<TextAsset>(ProductsCsvPath).text;
+         Products = ConvertCsv(csv);
 
          await LoadCurrencySprite();
       }
@@ -29,9 +31,7 @@ namespace ELTSDK.Source.Services.EditorServices
       public void Purchase(string id, bool withConsume) =>
          PurchaseComplete?.Invoke(id);
 
-      public void CheckPurchase(string id, bool withConsume)
-      {
-      }
+      public void CheckPurchase(string id, bool withConsume) { }
 
       private async UniTask LoadCurrencySprite()
       {
@@ -41,6 +41,33 @@ namespace ELTSDK.Source.Services.EditorServices
                await WebRequestService.Instance.DownloadTexture(Products.First().Value.CurrencyImageURL);
             CurrencySprite = currencyTexture.ToSprite();
          }
+      }
+
+      private Dictionary<string, Product> ConvertCsv(string csv)
+      {
+         var products = new Dictionary<string, Product>();
+         var raws = csv.Split('\n').Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
+         
+         for (int i = 1; i < raws.Count; i++)
+         {
+            var line = raws[i].Trim();
+            var columns = line.Split(';');
+            
+            var temp = new
+            {
+               Id = columns[0],
+               Price = columns[1],
+               Title = columns[2],
+               CurrencyImageURL = CurrencyURL
+            };
+            
+            var tempJson = JsonConvert.SerializeObject(temp);
+            var product = JsonConvert.DeserializeObject<Product>(tempJson);
+            
+            products.Add(product.Id, product);
+         }
+
+         return products;
       }
    }
 }
